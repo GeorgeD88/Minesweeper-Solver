8e6b24b01b457b6199f838d9b067b5f93e518116
George Doujaiji <GeorgeD.88888@gmail.com>
2021-05-30 00:16:30 -0400

    Change std.array to 2d lists and fix print board
    I changed all uses of std arrays for the matrices to lists of lists.
    Then I tried running the program but the boards were printing all over
    the place so I added end="" to every print statements and then the
    program started working perfectly! It generates the bombs and then
    calculates each regular tile's number of surrounding bombs.

--- minesweeper_v2.py
+++ minesweeper_v2.py
@@ -1,11 +1,6 @@
-#-----------------------------------------------------------------------
-# minesweeper.py
-#-----------------------------------------------------------------------
-
-import stdio
-import stdarray
-import sys
 import random
+import sys
+
 
 # Accept integers rows and cols, and float prob as command-line arguments.
 # Create a rows x cols minesweeper game where each cell is a bomb with
@@ -17,7 +12,7 @@ cols = int(input('columns: '))
 prob = float(input('bomb probability: '))
 
 # Create bombs as a rows+2 * cols+2 array.
-bombs = stdarray.create2D(rows+2, cols+2, False)
+bombs = [[False for i in range(cols+2)] for j in range(rows+2)]
 
 # bombs is [1..rows][1..cols]; the border is used to handle boundary cases.
 for r in range(1, rows+1):
@@ -28,13 +23,13 @@ for r in range(1, rows+1):
 for r in range(1, rows+1):
     for c in range(1, cols+1):
         if bombs[r][c]:
-            print('* ')
+            print('* ', end="")
         else:
-            print('. ')
+            print('. ', end="")
     print()
 
 # Create solution as a rows+2 x cols+2 array.
-solution = stdarray.create2D(rows+2, cols+2, 0)
+solution = [[0 for i in range(cols+2)] for j in range(rows+2)]
 
 # solution[i][j] is the number of bombs adjacent to cell (i, j).
 for r in range(1, rows+1):
@@ -50,9 +45,9 @@ print()
 for r in range(1, rows+1):
     for c in range(1, cols+1):
         if bombs[r][c]:
-            print('* ')
+            print('* ', end="")
         else:
-            print(str(solution[r][c]) + ' ')
+            print(str(solution[r][c]) + ' ', end="")
     print()
 
 #-----------------------------------------------------------------------
