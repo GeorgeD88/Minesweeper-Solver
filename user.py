bc0231de8f0c72f025d17b8b92197c16de0341cb
George Doujaiji <GeorgeD.88888@gmail.com>
2022-02-25 07:14:47 -0500

    Completely refactor input script & add new options
    I completely redid the input script and made it super nice. I didn't
    completely cover all input checking (like looping till the input is ok)
    only because the point of the project isn't for humans to play it, it's
    for the bot. Although tbh now I think imma just do the input checking.
    I also added some cool new options like play again and edit settings so
    you can change the dimensions and probability without having to restart.

--- user.py
+++ user.py
@@ -12,84 +12,97 @@ class User(Minesweeper):
     def play(self):
         """ Starts game loop. """
         last_move = ''
-        print('\n' * SPACER)
+        space()
 
-while True:
+        while True:
 
-    try:
-        print(last + '\n')
-        game.display_mask()  # displays game to user
-        print('\ninput format: mode row column\nreveal: R | F | M | Q')
+            # catches all errors so game doesn't crash and logs them to error.txt
+            try:
+                # prints last move, mask, and input guide
+                print(f'last move: {last_move}\n')
+                self.display_mask()  # displays game to user
+                print('\ninput format: mode row column\nreveal: R | F | M | Q')
 
-        # gets input
-        choice_str = input('\n')
-        choice = choice_str.split()
-        mode = choice.pop(0).lower()
-        if mode == 'q':
-            break
-        row, col = map(int, choice)
-        row -= 1
-        col -= 1
-        print()
-
-        # input checking to ensure it's within bounds
-        while not game.bounds(row, col):
-            print('selection out of bounds\n')
-            choice = input('\n').split()
-            mode = choice.pop(0)
-            row, col = map(int, choice)
-            row -= 1
-            col -= 1
-
-
-        # acts on choices (r, f, m, h, q respectively to reveal, flag or flag maybe a tile, to get help or to quit), optionally followed by coordinates
-        if mode == 'r':
-            # checks if choice was a mine and ends game
-            if game.game[row][col] is True:
-                game.display_game()
-                lose_message()
-                end_choice = input().lower()
-                if end_choice == 'p':
-                    pass
-                elif end_choice ==
-                break
-            # if no mine then continue with revealing square
-            else:
-                game.reveal(row, col)
-                cell_count = game.rows * game.cols
-                count = 0
-                for r in range(game.rows):
-                    for c in range(game.cols):
-                        if type(game.mask[r][c]) is int:
-                            count += 1
-                if cell_count - count == game.mine_count:
-                    print('\n'*30)
-                    game.display_game()
-                    print('🅱️'*(game.cols*2))
-                    print('🅱️'*(game.cols*2))
-                    print(' boi really won like that 😐 '.center(game.cols*2, '🅱'))
-                    print('🅱️'*(game.cols*2))
-                    print('🅱️'*(game.cols*2))
+                # gets input
+                choice_str = last_move = input('\n')
+                choice = choice_str.split()
+                mode = choice.pop(0).lower()
+                if mode == 'q':
                     break
-        elif mode == 'f':
-            game.flag(row, col)
-        elif mode == 'm':
-            game.maybe(row, col)
-        last_move = choice_str
+                row, col = map(int, choice)
+                row -= 1
+                col -= 1
+                print()
 
-        print('\n'*30)
+                # input checking to ensure it's within bounds
+                while not self.bounds(row, col):
+                    print('selection out of bounds\n')
+                    choice = input('\n').split()
+                    mode = choice.pop(0).lower()
+                    if mode == 'q':
+                        break
+                    row, col = map(int, choice)
+                    row -= 1
+                    col -= 1
+                    print()
 
-    except Exception as e:
-        with open('error.txt', 'w+') as error_file:
-            error_file.write(str(e))
-        print('💩'*game.cols)
-        print('💩'*game.cols)
+                # if Q is selected in redo loop, it wil break there so this is to break outer loop
+                if mode == 'q':
+                    break
+
+                # acts on choices: R | F | M | Q
+                if mode == 'r':
+                    # checks if choice was a mine (and mask is unexplored) and ends game
+                    if self.mask[row][col] is False and self.game[row][col] is True:
+                        space()
+                        self.display_game(border=True)
+                        lose_message()
+                        end_choice = input().lower()
+                        if end_choice == 'p':  # play again
+                            self.reset_game()
+                        elif end_choice == 'e':  # edit settings
+                            print()
+                            last_move = ''
+                            rows, cols, prob = get_options()
+                            self.set_up_game(rows, cols, prob)
+                        elif end_choice == 'q':  # quits game
+                            break
+                    # if no mine then continue with revealing square
+                    else:
+                        self.reveal(row, col)
+                        if self.iswin():  # if there's nothing more to be explored, it's a win
+                            space()
+                            self.display_game(border=True)
+                            win_message()
+                            end_choice = input().lower()
+                            if end_choice == 'p':  # play again
+                                self.reset_game()
+                            elif end_choice == 'e':  # edit settings
+                                print()
+                                last_move = ''
+                                rows, cols, prob = get_options()
+                                self.set_up_game(rows, cols, prob)
+                            elif end_choice == 'q':  # quits game
+                                break
+                elif mode == 'f':
+                    self.flag(row, col)
+                elif mode == 'm':
+                    self.maybe(row, col)
+                space()
+
+            except Exception as e:
+                with open('error.txt', 'w+') as error_file:
+                    error_file.write(str(e))
+                print('~~ error logged to file ~~')
 
 
 def get_options():
     """ Gets game options: rows, columns, and mine probability. """
     options_input = input('format: rows  columns  probability(optional)\n').split()
     return int(options_input[0]), int(options_input[1]), float(options_input[2])
+
+def space():
+    print('\n'*SPACER)
 
 def welcome_message():
     print("""
@@ -119,15 +132,15 @@ def lose_message():
     print("""
 
 
-            ===============
-            == GAME OVER ==
-            ===============
+               ===============
+               == GAME OVER ==
+               ===============
 
   (P) play again (Q) quit (E) edit settings
             """)
 
-
-welcome_message()
-rows, cols, prob = get_options()
-game = Minesweeper(rows, cols, prob)
-game.play()
+def play_game():
+    welcome_message()
+    print('           ', end='')  # prints a spacer to push get options message under welcome message
+    rows, cols, prob = get_options()
+    return User(rows, cols, prob)
