aac674cb63780704fdf61b00e547fdbdbffc9794
George Doujaiji <GeorgeD.88888@gmail.com>
2022-02-24 22:07:02 -0500

    Revise, comment code, and add functions (1/2)
    I revised the first half of the code and condensed some of it as well
    and very thoroughly commented it. I also realized there were some spots
    that could've been improved with functions so I added those too.

--- game.py
+++ game.py
@@ -1,134 +1,132 @@
 from copy import deepcopy
 import random
+from re import T
 # FIXME: when you flag and unflag a number it disappears
 
-# Minesweeper class to create objects representing a whole game with the boards.
-# This class allows me to easily run methods on the game instead of using global variables.
 class Minesweeper:
 
     def __init__(self, rows: int, cols: int, mine_spawn: float, chars_config: dict = None):
         # config dictionary that holds the actual character strings mappings
-        # for what character to display for each board element; e.g. bomb = 'X'
+        # for what character to display for each board element; e.g. mine = 'X'
         self.chars = {'tile': '□',
-                      'bomb': '🅱️',  # *
+                      'mine': '🅱️',  # *
                       'zero': ' ',
                       'flag': '+',
                       'maybe': '?'
                       } if chars_config is None else chars_config
         # █🅱️
-        #(r, a, d, h, q respectively to reveal, arm or disarm a tile, to get help or to quit), optionally followed by coordinates
+        #(R, A, D, H, Q respectively to reveal, arm or disarm a tile, to get help or to quit), optionally followed by coordinates
 
         # settings for the game board
         self.rows = rows
         self.cols = cols
         self.prob = mine_spawn
 
+        # board guides
+        self.rguide = self.make_guide(rows)
+        self.cguide = self.make_guide(cols)
+
         # the different boards (external viewed by player and internal only viewed by code)
-        self.bombs = self.create_bomb_board()  # just the bombs, stored in code form
-        self.game = self.create_game_board()  # internal board, stored in code form
-        self.mask = self.create_mask_board()  # the board as seen by the user, stored as strings
+        self.mines = self.gen_mine_board()  # just the mines, stored in code form
+        self.game = self.gen_game_board()  # internal board, stored in code form
+        self.mask = self.gen_mask_board()  # the board as seen by the user, stored as strings
 
-        self.bomb_count = 0
-        for bomb_row in self.bombs:
-            self.bomb_count += bomb_row.count(True)
+        # goes through the newly created mine board and counts number of mines generated
+        self.mine_count = 0
+        for mine_row in self.mines:
+            self.mine_count += mine_row.count(True)
 
-    # creates a bomb board: tiles (False) and bombs (True)
-    def create_bomb_board(self) -> list:
-        # puts random bombs everywhere including border
-        new_bomb_board = [[random.random() < self.prob for i in range(self.cols+2)] for j in range(self.rows+2)]
+    def gen_mine_board(self) -> list:
+        """ Generates a mine board based on probability given. (just True and False) """
+        return [[random.random() < self.prob for i in range(self.cols)] for j in range(self.rows)]
 
-        # overwrites the border's True/False mixed bombs with just False so that it doesn't mess up the count
-        for c in range(self.cols+2):  # top and bottom row
-            new_bomb_board[0][c] = False
-            new_bomb_board[-1][c] = False
-        for r in range(1, self.rows+1):  # left and right columns besides the overlap
-            new_bomb_board[r][0] = False
-            new_bomb_board[r][-1] = False
-        return new_bomb_board
+    def gen_game_board(self) -> list:
+        """ Generates a gameboard by overwriting a copy of the mine board. (adds mine counts) """
+        game_board = deepcopy(self.mines)  # deep copies mine board to write over it
 
-    # creates a game board from a previously created bomb board: tiles (int of adjacent bombs) and bombs (True)
-    def create_game_board(self) -> list:
-        # defines the board by starting with a copy of the bomb board that it can edit
-        game_board = deepcopy(self.bombs)
-        #board = [[0 for i in range(c+2)] for j in range(r+2)]
-        # goes through every element and replaces every regular
-        # tile with the number of bombs in the adjacent cent tiles.
-        for r in range(1, self.rows+1):
-            for c in range(1, self.cols+1):
-                if game_board[r][c] is False:  # meaning it's not a bomb
-                    game_board[r][c] = 0
-                    # (rr, cc) indexes neighboring cells.
+        for r in range(self.rows):
+            for c in range(self.cols):
+                # for non-mine tiles (number tiles)
+                if game_board[r][c] is False:
+                    game_board[r][c] = 0  # sets counter to 0
+                    # (rr, cc) indexes neighboring tiles and counts mines
                     for rr in range(r-1, r+2):
                         for cc in range(c-1, c+2):
-                            cell = self.bombs[rr][cc]
-                            if cell:
-                                game_board[r][c] += 1
+                            if self.bounds(rr, cc):  # if cell is within bounds
+                                # increments main tile if that surrounding tile has a mine
+                                if self.mines[rr][cc]:
+                                    game_board[r][c] += 1
+
         return game_board
 
-    # creates a mask board to display to the user: tiles, armed flags, maybe flags, etc. (strings)
-    def create_mask_board(self) -> list:
+    def gen_mask_board(self) -> list:
+        """ Generates a mask board to display to the user: tiles, armed flags, maybe flags, etc. (strings) """
         return [[False for i in range(self.cols)] for j in range(self.rows)]
 
-    def display_bombs(self):
-        for r in range(1, self.rows+1):
-            for c in range(1, self.cols+1):
-                print(self.bombs[r][c] + ' ', end='')
-            print()
+    def display_mines(self, ascii: bool = False):
+        """ Iterates through mine board and prints mine status. """
+        if ascii:  # prints mines as their mask symbol
+            for r in range(self.rows):
+                for c in range(self.cols):
+                    print(self.chars['mine'] if self.mines[r][c] else self.chars['tile'], end=' ')
+                print()  #                        do I print it as empty tile or as zero ___^
+        if not ascii:  # prints mines as boolean values
+            for r in range(self.rows):
+                for c in range(self.cols):
+                    print(self.mines[r][c], end=' ')
+                print()
 
     def display_game(self):
-        for r in range(1, self.rows+1):
-            for c in range(1,self.cols+1):
-                cell = self.game[r][c]
-                if cell is True:
-                    print(self.chars['bomb'] + ' ', end='')
-                elif cell == 0:
-                    print(self.chars['zero'] + ' ', end='')
-                elif type(cell) is int:
-                    print(str(cell) + ' ', end='')
+        """ Iterates through game board and prints tiles. (so numbers and mines) """
+        for r in range(self.rows):
+            for c in range(self.cols):
+                tile = self.game[r][c]
+                if tile is True:  # mine
+                    print(self.chars['mine'], end=' ')
+                elif tile == 0:  # empty tile (zero)
+                    print(self.chars['zero'], end=' ')
+                elif type(tile) is int:  # number tile
+                    print(str(tile), end=' ')
             print()
 
     def display_mask(self):
-        repeat = [1, 2, 3, 4, 5, 6, 7, 8, 9, '█']  # rename █ with the value from the dict
-        rem = self.cols % 10
-        guide = []
-        for i in range(int(self.cols-rem)//10):
-            guide.extend(repeat)
-        for i in range(rem):
-            guide.append(repeat[i])
+        """ Iterates through mask board and prints tiles. (what user sees) """
+        print('   ', end='')  # the little corner spacer before the X-axis guide
 
-        print('   ', end='')
-        for num in guide:
-            print(str(num) + ' ', end='')
+        # == X-AXIS ==
+
+        # prints the numbers first
+        for num in self.cguide:
+            print(str(num), end=' ')
         print()
+
+        # then prints the little ticks
         print('   ', end='')
         for i in range(self.cols):
             print('| ', end='')
         print()
 
-        rem = self.rows % 10
-        guide = []
-        for i in range(int(self.rows-rem)//10):
-            guide.extend(repeat)
-        for i in range(rem):
-            guide.append(repeat[i])
+        # == Y-AXIS & BOARD ==
 
         for r in range(self.rows):
-            print(f'{guide[r]}--', end='')
+            print(f'{self.cguide[r]}--', end='')  # prints the guide: number + tick
+            # goes through every tile in the row and gets mask symbol
             for c in range(self.cols):
-                cell = self.mask[r][c]
-                if cell is True:
-                    print(self.chars['bomb'] + ' ', end='')
-                elif cell is False:
-                    print(self.chars['tile'] + ' ', end='')
-                elif cell == 0:
-                    print(self.chars['zero'] + ' ', end='')
-                elif type(cell) is int:
-                    print(str(cell) + ' ', end='')
-                else:
-                    print(cell + ' ', end='')
+                tile = self.mask[r][c]
+                if tile is True:  # mine
+                    print(self.chars['mine'], end=' ')
+                elif tile is False:  # unexplored tile
+                    print(self.chars['tile'], end=' ')
+                elif tile == 0:  # empty tile (zero)
+                    print(self.chars['zero'], end=' ')
+                elif type(tile) is int:  # number tile
+                    print(str(tile), end=' ')
+                else:  # other chars: flag, maybe, etc.
+                    print(tile, end=' ')
             print()
 
     def cascade(self, r, c):
+        """ Flood fill algorithm. """
         for rr in range(r-1, r+2):
             for cc in range(c-1, c+2):
                 checking = self.game[rr][cc]
@@ -146,12 +144,12 @@ class Minesweeper:
 
     # checks the pressed tile and decides if it's a regular tile and then proceeds as needed
     def reveal(self, r, c):
-        cell = self.game[r][c]
-        self.mask[r-1][c-1] = cell
-        if cell == 0:
+        tile = self.game[r][c]
+        self.mask[r-1][c-1] = tile
+        if tile == 0:
             self.cascade(r, c)
 
-    # allows the user to flag unopened tiles as a bomb if speculated to be
+    # allows the user to flag unopened tiles as a mine if speculated to be
     def flag(self, r, c):
         if self.mask[r-1][c-1] == self.chars['flag']:  # unflags if already flagged
             self.mask[r-1][c-1] = False
@@ -163,3 +161,23 @@ class Minesweeper:
             self.mask[r-1][c-1] = False
         else:
             self.mask[r-1][c-1] = self.chars['maybe']  # maybes
+
+    def make_guide(self, length: int):
+        """ Makes board guide based on given length. """
+        repeat = [1, 2, 3, 4, 5, 6, 7, 8, 9, '█']  # █ is the 10s marker
+        rem = length % 10
+        guide = []
+
+        for i in range(length//10):  # extends the guide by 10 everytime
+            guide.extend(repeat)
+        for i in range(rem):  # extends the last bit that doesn't quite make 10
+            guide.append(repeat[i])
+
+        return guide
+
+    def bounds(self, r, c) -> bool:
+        """ Checks if given coord is within board bounds. """
+        if r < 0 or r >= self.rows or c < 0 or c >= self.rows:  # ripped from Guha's water.c floodfill function
+            return False  # meaning the coord is outside of the board
+        else:
+            return True  # coord is within the board
