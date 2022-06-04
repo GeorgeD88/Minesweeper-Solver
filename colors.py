4bf25360d1ac41cb57f44a02adcb015a9ad34e07
George Doujaiji <GeorgeD.88888@gmail.com>
2022-06-03 20:08:25 -0400

    Add color list and color test function

--- colors.py
+++ colors.py
@@ -7,3 +7,19 @@ RED = '\033[91m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 END_COLOR = '\033[0m'
+
+COLOR_LIST = [
+    '\033[95m',
+    '\033[94m',
+    '\033[96m',
+    '\033[92m',
+    '\033[93m',
+    '\033[91m',
+    '\033[1m',
+    '\033[4m'
+]
+
+def test_all_colors():
+    """ Prints a test string in every color. """
+    for c in COLOR_LIST:
+        print(c + 'Hello World!' + END_COLOR)
