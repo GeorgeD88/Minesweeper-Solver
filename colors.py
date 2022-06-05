99124f310a76a03700d6a9d7a5fe24044642fbb4
George Doujaiji <GeorgeD.88888@gmail.com>
2022-06-05 13:17:25 -0400

    Add hashmap from color to color name

--- colors.py
+++ colors.py
@@ -8,6 +8,18 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 END_COLOR = '\033[0m'
 
+color_names = {
+    '\033[95m': 'PURPLE',
+    '\033[94m': 'BLUE',
+    '\033[96m': 'CYAN',
+    '\033[92m': 'GREEN',
+    '\033[93m': 'YELLOW',
+    '\033[91m': 'RED',
+    '\033[1m': 'BOLD',
+    '\033[4m': 'UNDERLINE',
+    '\033[0m': 'END_COLOR'
+}
+
 COLOR_LIST = [
     '\033[95m',
     '\033[94m',
