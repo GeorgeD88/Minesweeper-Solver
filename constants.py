b948a42a4a20b7d72d65fc478b8a3d9967290c0a
George Doujaiji <GeorgeD.88888@gmail.com>
2022-11-27 04:23:32 -0500

    Store file's global constants on external file

---
+++ constants.py
@@ -0,0 +1,8 @@
+""" This is store global constants that are used among all Minesweeper files. """
+
+# == FRONTEND CONSTANTS ==
+WIN_HEIGHT = 900  # window height
+WAIT = 0.0015  # in seconds
+
+# == BACKEND CONSTANTS ==
+ADJACENT_COORDS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
