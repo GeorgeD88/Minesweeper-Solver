9259aca159bb26b8474f05deddb99d2d61e1fee1
George Doujaiji <GeorgeD.88888@gmail.com>
2022-12-03 00:41:43 -0500

    Edit comments in CLI minesweeper file

--- cli_ms.py
+++ cli_ms.py
@@ -1,11 +1,10 @@
-import cli_solver
 import cli_user
+import cli_solver
 
+# to play Minesweeper
+game = cli_user.init_game()
+game.play()  # user + game
 
-# Minesweeper game
-# game = user.init_game()
-# game.play()  # user + game
-
-# Minesweeper bot
+# to watch the bot play Minesweeper
 bot = cli_solver.init_solver()
 bot.solve()  # solver + game
