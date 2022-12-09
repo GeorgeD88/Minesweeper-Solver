650bff96c17118f8473c87d8dad866b9093d59c6
George Doujaiji <GeorgeD.88888@gmail.com>
2022-12-09 12:09:06 -0500

    Update colors

--- gui_colors.py
+++ gui_colors.py
@@ -10,6 +10,7 @@ DARK_GRAY_LOSS = (46, 46, 46)
 # solids
 RED = (224, 20, 20)
 ORANGE = (255, 165, 0)
+DULL_ORANGE = (181, 125, 40)
 GREEN = (60, 240, 90)
 TURQUOISE = (49, 208, 232)
 VIOLET = (195, 59, 219)
@@ -19,18 +20,23 @@ SOFT_ORANGE = (255, 179, 71)
 YELLOW = (242, 252, 131)
 GRAY_BLUE = (109, 139, 153)  # similar to soft blue, but grayed out
 SOFT_BLUE = (171, 215, 235)  # nice soft blue
+SKY_BLUE = (39, 128, 176)
 CYAN = (131, 252, 224)  # softish cyan
 SOFT_PURPLE = (235, 191, 255)  # nice soft purple
 
 # darker colors
+REDDISH_BROWN = (120, 56, 13)
 DARK_RED = (163, 18, 8)
 DARK_RED_TINT = (70, 18, 18)
+DARK_PURPLE = (69, 21, 117)
 
 # solver colors, TODO: try lightening the tints a bit more
 DARK_YELLOW_TINT = (70, 70, 18)  # traversed but not solved
-DARK_YELLOW_TINT2 = (122, 122, 26)  # traversed but not solved
+DARK_YELLOW_TINT2 = (122, 122, 26)
+YELLOW_TINT = (171, 171, 41)
 DARK_GREEN_TINT = (18, 70, 18)  # fully solved tiles
-DARK_GREEN_TINT2 = (22, 94, 22)  # fully solved tiles
+DARK_GREEN_TINT2 = (22, 94, 22)
+GREEN_TINT = (46, 158, 46)
 DARK_PURPLE_TINT = (70, 24, 79)  # current node color
 DULL_BLUE = (27, 125, 158)  # lake scan, traversing pools
 DULL_BLUE_LEADING = (33, 172, 219)
